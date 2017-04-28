1271354e684b36a8225ba595024f262bcb55adeb
kornel.zemla <kornel.zemla@ocado.com>
2017-04-28 16:39:18 +0200

    Enc refactoring

--- src/main/java/kze/backup/glacier/encrypt/OpenSslAes.java
+++ src/main/java/kze/backup/glacier/encrypt/OpenSslAes.java
@@ -24,52 +24,33 @@ public class OpenSslAes {
     private static final String SALTED_STR = "Salted__";
     private static final byte[] SALTED_MAGIC = SALTED_STR.getBytes(US_ASCII);
 
-    public String encrypt(String password, String clearText) throws Exception {
-        final byte[] pass = password.getBytes(US_ASCII);
-
-        final byte[] salt = (new SecureRandom()).generateSeed(8);
-
-        final byte[] inBytes = clearText.getBytes(UTF_8);
-
-        final byte[] passAndSalt = array_concat(pass, salt);
-        byte[] keyAndIv = generateKeyAnIV(passAndSalt);
-
-        final byte[] keyValue = Arrays.copyOfRange(keyAndIv, 0, 32);
-        final byte[] iv = Arrays.copyOfRange(keyAndIv, 32, 48);
-        final SecretKeySpec key = new SecretKeySpec(keyValue, "AES");
-
-        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
-        byte[] data = cipher.doFinal(inBytes);
-        data = array_concat(array_concat(SALTED_MAGIC, salt), data);
-        return Base64.getEncoder().encodeToString(data);
+    public String encrypt(String password, String textToEncode) throws Exception {
+        final byte[] inBytes = textToEncode.getBytes(UTF_8);
+        byte[] outBytes = encrypt(password, inBytes);
+        return Base64.getEncoder().encodeToString(outBytes);
     }
 
-    public String decrypt(String password, String source) throws Exception {
-        final byte[] pass = password.getBytes(US_ASCII);
+    public byte[] encrypt(String password, byte[] bytesToEncode) throws Exception {
+        final byte[] salt = (new SecureRandom()).generateSeed(8);
+        final Cipher cipher = getCipher(password, salt, Cipher.ENCRYPT_MODE);
+        byte[] data = cipher.doFinal(bytesToEncode);
+        return array_concat(array_concat(SALTED_MAGIC, salt), data);
+    }
 
-        final byte[] inBytes = Base64.getDecoder().decode(source);
+    public String decrypt(String password, String textToDecrypt) throws Exception {
+        final byte[] inBytes = Base64.getDecoder().decode(textToDecrypt);
+        byte[] openBytes = decrypt(password, inBytes);
+        return new String(openBytes, UTF_8);
+    }
 
-        final byte[] shouldBeMagic = Arrays.copyOfRange(inBytes, 0, SALTED_MAGIC.length);
+    public byte[] decrypt(String password, byte[] bytesToDecode) throws Exception {
+        final byte[] shouldBeMagic = Arrays.copyOfRange(bytesToDecode, 0, SALTED_MAGIC.length);
         if (!Arrays.equals(shouldBeMagic, SALTED_MAGIC)) {
             throw new IllegalArgumentException("Initial bytes from input do not match OpenSSL SALTED_MAGIC salt value.");
         }
-
-        final byte[] salt = Arrays.copyOfRange(inBytes, SALTED_MAGIC.length, SALTED_MAGIC.length + 8);
-
-        final byte[] passAndSalt = array_concat(pass, salt);
-
-        byte[] keyAndIv = generateKeyAnIV(passAndSalt);
-
-        final byte[] keyValue = Arrays.copyOfRange(keyAndIv, 0, 32);
-        final SecretKeySpec key = new SecretKeySpec(keyValue, "AES");
-
-        final byte[] iv = Arrays.copyOfRange(keyAndIv, 32, 48);
-
-        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
-        final byte[] clear = cipher.doFinal(inBytes, 16, inBytes.length - 16);
-        return new String(clear, UTF_8);
+        final byte[] salt = Arrays.copyOfRange(bytesToDecode, SALTED_MAGIC.length, SALTED_MAGIC.length + 8);
+        final Cipher cipher = getCipher(password, salt, Cipher.DECRYPT_MODE);
+        return cipher.doFinal(bytesToDecode, 16, bytesToDecode.length - 16);
     }
 
     private byte[] generateKeyAnIV(byte[] passAndSalt) throws NoSuchAlgorithmException {
@@ -82,6 +63,18 @@ public class OpenSslAes {
             keyAndIv = array_concat(keyAndIv, hash);
         }
         return keyAndIv;
+    }
+
+    private Cipher getCipher(String password, byte[] salt, int cypherMode) throws Exception {
+        final byte[] pass = password.getBytes(US_ASCII);
+        final byte[] passAndSalt = array_concat(pass, salt);
+        byte[] keyAndIv = generateKeyAnIV(passAndSalt);
+        final byte[] keyValue = Arrays.copyOfRange(keyAndIv, 0, 32);
+        final SecretKeySpec key = new SecretKeySpec(keyValue, "AES");
+        final byte[] iv = Arrays.copyOfRange(keyAndIv, 32, 48);
+        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(cypherMode, key, new IvParameterSpec(iv));
+        return cipher;
     }
 
     private byte[] array_concat(final byte[] a, final byte[] b) {
