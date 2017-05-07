a8f3bdb9b1314ddd161f53ae0c3fa8a1da09db4c
Kornel Zemla <kornelzemla@gmail.com>
2017-05-07 20:18:21 +0200

    +log

--- src/main/java/kze/backup/glacier/MD5.java
+++ src/main/java/kze/backup/glacier/MD5.java
@@ -6,14 +6,20 @@ import java.nio.file.Path;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import static java.awt.SystemColor.info;
+import static java.lang.System.exit;
+import static kze.backup.glacier.Logger.error;
+import static kze.backup.glacier.Logger.info;
+
 public class MD5 {
 
     public static String digest(Path path) {
+        info("Computing md5 for [%s]", path);
         try (InputStream inputStream = Files.newInputStream(path)) {
             return DigestUtils.md5Hex(inputStream);
         } catch (Exception e) {
-            Logger.error("Unable to read file=[%s]", e, path);
-            System.exit(-1);
+            error("Unable to read file=[%s]", e, path);
+            exit(-1);
         }
         return "";
     }
